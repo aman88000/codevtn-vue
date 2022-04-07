c2abb67c6fc8386fe685beaa405b96bfe30d0c0d
aman88000 <aman88000@gmail.com>
2022-04-07 18:56:28 +0530

    Emit validattion warn in console to user its valid or not

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -2,17 +2,32 @@
     <div>
         <h1>Popup Model component</h1>
         <!-- <button @click="passFun('Testing Child Data')">Close Popup</button> -->
-        <button @click="$emit('close', 'aman')">Close Popup</button>
+        <input type="text" v-model="name" />
+        <button @click="$emit('close', name)">Close Popup</button>
     </div>
 </template>
 
 <script>
     export default {
         name: 'PopupVue',
+        data(){
+            return{
+                name: ''
+            }
+        },
+
         // props:{
         //     passFun: Function
-        // }
-        emits: ['close']
+        // },
+        emits: {
+            close(value){
+                if(!value){
+                    return false
+                }else{
+                    return true
+                }
+            }
+        }
     }
 </script>
 
